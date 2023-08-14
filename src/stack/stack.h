0be5ad7c67e4011b6b02f0dfdd8aa13c3600af93
hinpa <masterz4pl@gmail.com>
2023-08-14 20:51:57 +0300

    Fixed bug in equals operator overloading
    this bug reversed the order in the resulting stack when copying

--- src/stack/stack.h
+++ src/stack/stack.h
@@ -35,17 +35,48 @@ Stack<Type>::Stack(const Stack &copy)
 {
     *this = copy;
 }
-
+// stack_int = stack_int_2.
 template<typename Type>
 Stack<Type>& Stack<Type>::operator=(const Stack &copy)
 {
-    free();
-    Node *copy_iterator = copy.HEAD;
-    while (copy_iterator != NULL)
+    if (copy.HEAD == NULL) 
     {
-        push(copy_iterator->data);
+        free();
+        return *this;
+    }
+    if (HEAD == NULL)
+        HEAD = new Node;
+    Node *copy_iterator = copy.HEAD;
+    Node *it = HEAD;
+    size = copy.size;
+    // copy until we meet last element of either stack that being copied
+    // or stack that we are copying into
+    while (it->next && copy_iterator->next)
+    {
+        it->data = copy_iterator->data;
+        it = it->next;
         copy_iterator = copy_iterator->next;
     }
+    // last element is yet to be copied
+    it->data = copy_iterator->data;
+    copy_iterator = copy_iterator->next;
+    Node *free_it = it->next;
+    // in case there's nothing left to copy
+    while (free_it)
+    {
+        Node *delem = free_it;
+        free_it = free_it->next;
+        delete delem;
+    }
+    // in case there's still something to copy
+    while (copy_iterator)
+    {
+        it->next = new Node; // alloc new node
+        it = it->next; // move to it
+        it->data = copy_iterator->data; // copy 
+        copy_iterator = copy_iterator->next; // move next
+    }
+    it->next = NULL;
     return *this;
 }
 
@@ -53,9 +84,7 @@ template<typename Type>
 Type Stack<Type>::pop()
 {
     if (HEAD == NULL)
-    {
         throw std::out_of_range("Popping an empty stack!");
-    }
     Node *next_element = HEAD->next;
     Type retval = HEAD->data;
     delete HEAD;
@@ -81,6 +110,8 @@ Stack<Type> &Stack<Type>::push(Type data)
     return *this;
 }
 
+// frees space taken by stack structure
+// does not free elements stored in stack itself
 template<typename Type>
 void Stack<Type>::free()
 {
